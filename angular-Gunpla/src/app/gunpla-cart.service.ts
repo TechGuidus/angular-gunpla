43112f57cf8781f4f958f8c07963db45135c3266
TechGuidus <72898691+TechGuidus@users.noreply.github.com>
2020-12-04 17:01:05 -0300

    Added final changes
    Removed bugs added more comments.

--- angular-Gunpla/src/app/gunpla-cart.service.ts
+++ angular-Gunpla/src/app/gunpla-cart.service.ts
@@ -13,11 +13,14 @@ export class GunplaCartService {
   
   shopList: BehaviorSubject<Gunpla[]> = new BehaviorSubject([]);
 
-
+ //removefromCart (gunpla: Gunpla){
+// desarrollar funcion inversa para eliminar cosas del carro de compras, se seguira testeando en vacaciones.
+//
+//}
   
 
   addToCart(gunpla: Gunpla) {
-    let item:Gunpla = this._shopList.find((v1)=> v1.nombre = gunpla.nombre);
+    let item:Gunpla = this._shopList.find((v1)=> v1.nombre == gunpla.nombre);
     if (!item){
       this._shopList.push({...gunpla}); 
     }
